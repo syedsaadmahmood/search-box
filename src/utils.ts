73054fd4e922642d590222665d325b20ce2e9a9e
Saad Mahmood <s.saad.m@outlook.com>
2024-07-25 00:45:58 +0500

    fixed build issue

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,8 @@
-export function debounce<T extends (...args: any[]) => any>(func: T, wait: number): T {
-    let timeout: ReturnType<typeof setTimeout>;
-    return function(this: any, ...args: Parameters<T>) {
-      clearTimeout(timeout);
-      timeout = setTimeout(() => func.apply(this, args), wait);
-    } as T;
-  }
-  
+export function debounce<T extends (...args: any[]) => any>(func: T, wait: number): (...args: Parameters<T>) => void {
+  let timeout: ReturnType<typeof setTimeout>;
+
+  return function(this: any, ...args: Parameters<T>): void {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), wait);
+  };
+}
